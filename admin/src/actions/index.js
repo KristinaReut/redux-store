f0347a42f602a17f64598fa8abd1a2a6a344b0fb
Kristina <kristina241996@mail.ru>
2018-08-24 11:31:48 +0300

    admin => combine files

--- admin/src/actions/index.js
+++ admin/src/actions/index.js
@@ -1,13 +1,21 @@
-import * as types from '../constants/ActionTypes'
+export const ADD_CATEGORY = 'ADD_CATEGORY'
+export const DELETE_CATEGORY = 'DELETE_CATEGORY'
+export const EDIT_CATEGORY = 'EDIT_CATEGORY'
+export const GET_ALL_CATEGORIES = 'GET_ALL_CATEGORIES'
 
-export const addCategory = text => ({ type: types.ADD_CATEGORY, text })
-export const deleteCategory = id => ({ type: types.DELETE_CATEGORY, id })
-export const editCategory = (id, text) => ({ type: types.EDIT_CATEGORY, id, text })
-export const getAllCategories = () => ({ type: types.GET_ALL_CATEGORIES })
+export const ADD_PRODUCT = 'ADD_PRODUCT'
+export const DELETE_PRODUCT= 'DELETE_PRODUCT'
+export const EDIT_PRODUCT = 'EDIT_PRODUCT'
+export const GET_ALL_PRODUCTS = 'GET_ALL_PRODUCTS'
 
-export const addProduct = text => ({ type: types.ADD_PRODUCT, text })
-export const deleteProduct = id => ({ type: types.DELETE_PRODUCT, id })
-export const editProduct = (id, text) => ({ type: types.EDIT_PRODUCT, id, text })
-export const getAllProducts = () => ({ type: types.GET_ALL_PRODUCTS })
+export const addCategory = text => ({ type: ADD_CATEGORY, text })
+export const deleteCategory = id => ({ type: DELETE_CATEGORY, id })
+export const editCategory = (id, text) => ({ type: EDIT_CATEGORY, id, text })
+export const getAllCategories = () => ({ type: GET_ALL_CATEGORIES })
+
+export const addProduct = text => ({ type: ADD_PRODUCT, text })
+export const deleteProduct = id => ({ type: DELETE_PRODUCT, id })
+export const editProduct = (id, text) => ({ type: EDIT_PRODUCT, id, text })
+export const getAllProducts = () => ({ type: GET_ALL_PRODUCTS })
 
 
